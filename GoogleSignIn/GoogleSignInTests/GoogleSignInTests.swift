1109db7f3fa58dff4a059914ee596c83278b3c4a
An Luu <atluutran@gmail.com>
2025-02-04 09:46:03 -0500

    Created placeholder proj

---
+++ GoogleSignIn/GoogleSignInTests/GoogleSignInTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSignInTests.swift
+//  GoogleSignInTests
+//
+//  Created by An Luu on 2/4/25.
+//
+
+import Testing
+@testable import GoogleSignIn
+
+struct GoogleSignInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
